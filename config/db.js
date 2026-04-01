1da2cbd7257dab880752b3f8f60180e67beb253a
Vijit Singh <vijit.singh@intglobal.com>
2026-04-01 11:46:34 +0530

    db updated

--- config/db.js
+++ config/db.js
@@ -11,10 +11,10 @@ const mysql = require("mysql2");
 //   queueLimit: 0,
 // });
 const pool = mysql.createPool({
-  host: "192.168.1.205",
-  user: "lead_scoring_dev_db_user",
-  password: "EeeQC29goGSV",
-  database: "lead_scoring_dev_db",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
